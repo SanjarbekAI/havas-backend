15920250119b942d9ac307ef8cc008655a79860f
SanjarbekAI <sanjarbekwork@gmail.com>
2025-11-12 11:29:33 +0500

    docker added

--- core/config.py
+++ core/config.py
@@ -21,16 +21,16 @@ else:
 
 # DJANGO CORE SETTINGS
 SECRET_KEY = env('DJANGO_SECRET_KEY', default='unsafe-secret-key')
-DEBUG = env.bool('DEBUG', default=False)
+DEBUG = env.bool('DEBUG', default=True)
 ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS', default=['localhost', '127.0.0.1'])
 
 # DATABASE SETTINGS
-DB_NAME = env('DB_NAME')
-DB_USER = env('DB_USER')
-DB_PASS = env('DB_PASS')
-DB_HOST = env('DB_HOST')
-DB_PORT = env('DB_PORT')
+DB_HOST = env('DB_HOST', default='db')
+DB_NAME = env('DB_NAME', default='devdb')
+DB_USER = env('DB_USER', default='devuser')
+DB_PASS = env('DB_PASS', default='changeme')
+DB_PORT = env('DB_PORT', default=5432)
 
 # telegram bot
-TELEGRAM_BOT_TOKEN = env('TELEGRAM_BOT_TOKEN')
-TELEGRAM_CHANNEL_ID = env('TELEGRAM_CHANNEL_ID')
+TELEGRAM_BOT_TOKEN = env('TELEGRAM_BOT_TOKEN', default='token')
+TELEGRAM_CHANNEL_ID = env('TELEGRAM_CHANNEL_ID', default='id')
